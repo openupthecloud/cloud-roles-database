6af9d21d9561bbc7d33646b62c3c2af0b1edd43c
Lou Bichard <contact@louisjohnbichard.co.uk>
2022-07-21 06:23:04 +0000

    fix: move files to backend folder

--- Makefile
+++ Makefile
@@ -1,22 +1,22 @@
 
 # https://github.com/golang-migrate/migrate/blob/master/database/postgres/TUTORIAL.md
 create_migration: 
-	migrate create -dir db/migrations -ext sql temp
+	migrate create -dir backend/db/migrations -ext sql temp
 
 migrate_up:
-	migrate -database "postgres://postgres:password@0.0.0.0:5432/postgres?sslmode=disable" -path db/migrations up
+	migrate -database "postgres://postgres:password@0.0.0.0:5432/postgres?sslmode=disable" -path backend/db/migrations up
 
 migrate_down:
-	migrate -database "postgres://postgres:password@0.0.0.0:5432/postgres?sslmode=disable" -path db/migrations down
+	migrate -database "postgres://postgres:password@0.0.0.0:5432/postgres?sslmode=disable" -path backend/db/migrations down
 
 db_clean: 
 	psql postgres://postgres:password@0.0.0.0:5432/postgres -f sql/clean.sql
 
 build: 
-	go build -o main ./api
+	go build -o main ./backend/api
 
 run: 
 	./run.sh
 
 test: 
-	go test ./api -v
+	go test ./backend/api -v
--- backend/sql/queries.sql
+++ backend/sql/queries.sql
@@ -1,16 +1,48 @@
+SELECT 
+    jobs.url 
+FROM 
+    jobs_cache
+INNER JOIN
+    jobs
+ON
+    jobs.job_id = jobs_cache.job_id
+
+
 -- DUMP ALL TABLES
 SELECT * FROM skills; -- Includes the skill categories
 SELECT * FROM jobs;
+SELECT COUNT(*) FROM jobs_cache;
 SELECT COUNT(*) FROM jobs;
 SELECT * FROM job_skills;
 SELECT * FROM synonyms;
+
+-- CLEAN TABLE
+SELECT * FROM jobs
+
+DELETE FROM jobs_cache
+
+ALTER TYPE job_title ADD VALUE 'Software Engineer'; -- appends to list
+
+-- GET SPECIFIC JOB
+SELECT jobs.job_id, jobs.url, jobs.country, jobs.job_title, job_skills.skill
+FROM job_skills INNER JOIN jobs ON job_skills.job_id = jobs.job_id
+WHERE jobs.job_id = '7937887e-ac22-4525-b990-c579ad02a0c9'
 
 -- GET ALL JOBS + SKILLS
 SELECT jobs.job_id, jobs.country, jobs.job_title, job_skills.skill
 FROM job_skills INNER JOIN jobs ON job_skills.job_id = jobs.job_id;
 
--- COUNT TOTAL EXTRACTED JOB DESCRIPTIONS
-SELECT COUNT(*) FROM jobs;
+-- SANITISE NON CLOUD JOBS (jobs, jobs_cache, job_skills)
+DELETE FROM jobs WHERE job_id IN (
+    SELECT job_id FROM (
+        SELECT 
+            jobs.job_id, 
+            jobs.job_title, 
+            (SELECT COUNT(*) FROM job_skills WHERE job_skills.job_id = jobs.job_id AND job_skills.skill IN ('AWS', 'Azure', 'GCP', 'Kubernetes')) 
+        FROM jobs
+    ) outr
+    WHERE count = 0
+)
 
 -- COUNT JOB TITLES
 SELECT job_title, COUNT(*) FROM jobs GROUP BY job_title ORDER BY count DESC;
@@ -20,6 +52,65 @@ SELECT job_skills.skill, skills.category, COUNT(*) as total, cast(COUNT(*) * 100
 FROM job_skills
     INNER JOIN skills ON job_skills.skill = skills.skill
 WHERE skills.category NOT IN ('Practices')
+AND skills.skill NOT IN ('Java', 'JavaScript', 'High Availability', 'Go', 'Degree')
+GROUP BY job_skills.skill, skills, category
+ORDER BY total DESC;
+
+-- COUNT ROLE DIFF TO MAIN SKILLS
+SELECT * FROM (
+    SELECT 
+        *,
+        (role_percentage - cloud_percentage) as diff,
+        ABS((role_percentage - cloud_percentage)) as diff_abs
+    FROM (
+        SELECT 
+            job_skills.skill, 
+            COUNT(*) as total, 
+            inna.percentage as cloud_percentage,
+            cast(COUNT(*) * 100.0 / (select count(*) from jobs WHERE job_title = 'Cloud Operations') as integer) as role_percentage
+        FROM job_skills
+            INNER JOIN skills ON job_skills.skill = skills.skill
+            INNER JOIN jobs ON job_skills.job_id = jobs.job_id
+            INNER JOIN  (
+                SELECT job_skills.skill, cast(COUNT(*) * 100.0 / (select count(*) from jobs) as integer) as percentage
+                FROM job_skills
+                    INNER JOIN skills ON job_skills.skill = skills.skill
+                AND skills.skill = skills.skill
+                GROUP BY job_skills.skill, skills, category
+            ) as inna ON inna.skill = skills.skill
+        AND skills.skill NOT IN ('Java', 'JavaScript', 'High Availability', 'Go', 'Degree')
+        AND jobs.job_title = 'Cloud Operations'
+--      AND skills.category NOT IN ('Practices')
+        GROUP BY job_skills.skill, skills, skills.category, jobs.job_title, inna.percentage
+    ) as outr
+    ORDER BY diff_abs DESC
+) outrrr
+LIMIT 20
+
+-- SKILLS AS A DATA ENGINEER
+SELECT job_skills.skill, COUNT(*) as total, cast(COUNT(*) * 100.0 / (select count(*) from jobs WHERE job_title = 'Data Engineer') as integer) as percentage
+FROM job_skills
+    INNER JOIN skills ON job_skills.skill = skills.skill
+    INNER JOIN jobs ON job_skills.job_id = jobs.job_id
+AND skills.skill NOT IN ('Java', 'JavaScript', 'High Availability', 'Go', 'Degree')
+AND jobs.job_title = 'Data Engineer'
+AND skills.category NOT IN ('Practices')
+GROUP BY job_skills.skill, skills, category, jobs.job_title
+ORDER BY total DESC;
+
+-- COUNT MOST POPULAR SKILLS + PRACTICES
+SELECT job_skills.skill, skills.category, COUNT(*) as total, cast(COUNT(*) * 100.0 / (select count(*) from jobs) as integer) as percentage
+FROM job_skills
+    INNER JOIN skills ON job_skills.skill = skills.skill
+WHERE skills.skill NOT IN ('Java', 'JavaScript', 'High Availability', 'Go', 'Degree')
+GROUP BY job_skills.skill, skills, category
+ORDER BY total DESC;
+
+-- COUNT MOST POPULAR PRACTICES
+SELECT job_skills.skill, skills.category, COUNT(*) as total, cast(COUNT(*) * 100.0 / (select count(*) from jobs) as integer) as percentage
+FROM job_skills
+    INNER JOIN skills ON job_skills.skill = skills.skill
+WHERE skills.category IN ('Practices')
 GROUP BY job_skills.skill, skills, category
 ORDER BY total DESC;
 
@@ -29,6 +120,23 @@ FROM job_skills
     INNER JOIN skills ON job_skills.skill = skills.skill
 WHERE skills.category IN ('Databases')
 GROUP BY job_skills.skill, skills, category
+ORDER BY total DESC;
+
+-- COUNT SKILLS REQUIRING CODING
+SELECT COUNT(*) as total, cast(COUNT(*) * 100.0 / (select count(*) from job_skills) as integer) as percentage
+FROM job_skills
+    INNER JOIN skills ON job_skills.skill = skills.skill
+WHERE skills.category IN ('Language')
+ORDER BY total DESC;
+
+-- MONITORING SKILLS AS AN SRE
+SELECT job_skills.skill, COUNT(*) as total, cast(COUNT(*) * 100.0 / (select count(*) from jobs WHERE job_title = 'Site Reliability Engineer') as integer) as percentage
+FROM job_skills
+    INNER JOIN skills ON job_skills.skill = skills.skill
+    INNER JOIN jobs ON job_skills.job_id = jobs.job_id
+WHERE skills.category IN ('Monitoring')
+AND jobs.job_title = 'Site Reliability Engineer'
+GROUP BY job_skills.skill, skills, category, jobs.job_title
 ORDER BY total DESC;
 
 -- COUNT MOST POPULAR LANGUAGES
@@ -55,7 +163,15 @@ WHERE skills.category IN ('Monitoring')
 GROUP BY job_skills.skill, skills, category
 ORDER BY total DESC;
 
--- COUNT MOST POPULAR MONITORING
+-- COUNT MOST POPULAR CLOUD PLATFORM
+SELECT job_skills.skill, skills.category, COUNT(*) as total, cast(COUNT(*) * 100.0 / (select count(*) from jobs) as integer) as percentage
+FROM job_skills
+    INNER JOIN skills ON job_skills.skill = skills.skill
+WHERE skills.category IN ('Cloud Platform')
+GROUP BY job_skills.skill, skills, category
+ORDER BY total DESC;
+
+-- COUNT MOST POPULAR HIGH AVAILABILITY
 SELECT job_skills.skill, skills.category, COUNT(*) as total, cast(COUNT(*) * 100.0 / (select count(*) from jobs) as integer) as percentage
 FROM job_skills
     INNER JOIN skills ON job_skills.skill = skills.skill
@@ -73,21 +189,13 @@ AND jobs.job_title = 'Site Reliability Engineer'
 GROUP BY job_skills.skill, skills, category, jobs.job_title
 ORDER BY total DESC;
 
--- MONITORING SKILLS AS AN SRE
-SELECT job_skills.skill, COUNT(*) as total, cast(COUNT(*) * 100.0 / (select count(*) from jobs WHERE job_title = 'Security Engineer') as integer) as percentage
-FROM job_skills
-    INNER JOIN skills ON job_skills.skill = skills.skill
-    INNER JOIN jobs ON job_skills.job_id = jobs.job_id
-AND jobs.job_title = 'Security Engineer'
-GROUP BY job_skills.skill, skills, category, jobs.job_title
-ORDER BY total DESC;
-
 -- COUNT MOST POPULAR SKILLS (FOR CLOUD ENGINEER)
 SELECT job_skills.skill, COUNT(*) as total, cast(COUNT(*) * 100.0 / (select count(*) from jobs WHERE job_title = 'Cloud Engineer') as integer) as percentage
 FROM job_skills
     INNER JOIN skills ON job_skills.skill = skills.skill
     INNER JOIN jobs ON job_skills.job_id = jobs.job_id
 WHERE skills.category NOT IN ('Practices')
+AND skills.skill NOT IN ('Java', 'JavaScript', 'High Availability', 'Go', 'Degree')
 AND jobs.job_title = 'Cloud Engineer'
 GROUP BY job_skills.skill, skills, category, jobs.job_title
 ORDER BY total DESC;
@@ -173,7 +281,27 @@ SELECT * FROM (
     ) as main
 ) as outside
 WHERE skill NOT IN ('Java', 'JavaScript', 'High Availability', 'Go', 'Degree')
-AND skill IN ('Python')
+AND skill IN ('Security')
+AND PERCENTAGE NOT IN (100)
+ORDER BY percentage DESC
+
+-- COUNT JOBS THAT MENTION NETWORKING
+SELECT * FROM (
+    SELECT *, cast((cast (main.total_skill as float) / cast(main.total_jobs as float) * 100) as int) as percentage FROM (
+        SELECT *, (select count(*) as blah from jobs WHERE jobs.job_title = inside.job_title) as total_jobs FROM (
+            SELECT 
+                job_skills.skill,
+                job_title,
+                COUNT(job_skills.skill) as total_skill
+            FROM job_skills
+                INNER JOIN skills ON job_skills.skill = skills.skill
+                INNER JOIN jobs ON job_skills.job_id = jobs.job_id
+            GROUP BY job_title, job_skills.skill
+        ) as inside
+    ) as main
+) as outside
+WHERE skill NOT IN ('Java', 'JavaScript', 'High Availability', 'Go', 'Degree')
+AND skill IN ('Networking')
 AND PERCENTAGE NOT IN (100)
 ORDER BY percentage DESC
 
@@ -203,9 +331,9 @@ SELECT COUNT(*) as mentioned_aws_and_azure, cast(COUNT(*) * 100.0 / (select coun
     ) as temp
 WHERE 
     true = true
-    AND temp.aws = 1 
-    AND temp.azure = 1 
-    AND temp.gcp = 1
+AND (temp.aws = 1 AND temp.azure = 1)
+AND (temp.aws = 1 AND temp.gcp = 1)
+AND (temp.gcp = 1 AND temp.azure = 1)
 
 -- COUNT JOBS MENTIONING AWS LAMBDA OR KUBERNETES
 SELECT job_skills.skill, skills.category, COUNT(*), cast(COUNT(*) * 100.0 / (select count(*) from jobs) as integer) as percentage
@@ -260,5 +388,3 @@ GROUP BY job_title,
 -- COUNT ALL DUPLICATE JOBS
 SELECT * FROM jobs inr
 WHERE (SELECT count(*) FROM JOBS outr WHERE inr.url = outr.url) > 1
-
-
